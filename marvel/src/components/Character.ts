f68fd7bd84aa9721aa20135a46bfe7f87d022317
Ange-H0610 <hei.iraka.4@gmail.com>
2025-08-09 13:38:22 +0300

    Remove the comments

--- marvel/src/components/Character.ts
+++ marvel/src/components/Character.ts
@@ -2,5 +2,5 @@ export interface Character {
   id: number;
   name: string;
   realName: string;
-  universe: 'Marvel' | 'DC' | 'Autre'; // Limite les univers possibles
+  universe: 'Marvel' | 'DC' | 'Autre'; 
 }
